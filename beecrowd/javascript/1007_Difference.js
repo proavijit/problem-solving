9747a79518c8f3650f6381d3f2c82ae91f4971f2
proavijit <avijitghosh2268@gmail.com>
2026-01-18 23:37:48 +0600

    Solve Beecrowd 1007 - Difference (JavaScript)

---
+++ beecrowd/javascript/1007_Difference.js
@@ -0,0 +1,11 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split('\n');
+
+
+let A = parseInt(lines[0])
+let B = parseInt(lines[1])
+let C = parseInt(lines[2])
+let D = parseInt(lines[3])
+
+let DIFFERENCE = (A * B - C * D);
+console.log("DIFERENCA = " + DIFFERENCE)
